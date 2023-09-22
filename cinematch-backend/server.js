92bab8287c25dddbd9b220c07c7f5be83e7ecb3e
Georgesproat <georgesproat1@gmail.com>
2023-09-22 18:42:23 +1200

    post and put

--- cinematch-backend/server.js
+++ cinematch-backend/server.js
@@ -1,17 +1,30 @@
 const express = require("express");
 const mongoose = require("mongoose");
-const userRoutes = require("./routes/userRoutes"); // Import user routes
+const mongoDBURI = "mongodb://127.0.0.1/CineMatch";
+const userRoutes = require("./routes/userRoutes");
 
 const app = express();
 
-// ... Other middleware and configuration ...
+mongoose.connect(mongoDBURI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+});
+
+// Middleware for parsing JSON request bodies
+app.use(express.json());
+
+const db = mongoose.connection;
+
+db.on("error", console.error.bind(console, "MongoDB connection error:"));
+db.once("open", () => {
+  console.log("Connected to MongoDB!");
+});
 
 // Mount user routes
 app.use("/api/user", userRoutes);
 
-// ... Other routes and server setup ...
-
 const PORT = process.env.PORT || 3000;
+
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
